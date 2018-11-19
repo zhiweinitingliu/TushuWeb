341794bbefd58a9db42c470b91a7a785de42a528
a1593 <a1593@DESKTOP-VG5DJ57.lan>
2018-11-19 22:07:06 +0800

    完成spring的配置

--- src/main/java/com/dukang/tushu/controller/UserController.java
+++ src/main/java/com/dukang/tushu/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-	@RequestMapping("/hello")
+	@RequestMapping("/hello")//请求的映射，和浏览器请求的url一致
     public void hello()
     {
         System.out.println("hello word");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,6 @@
+<%@page language="java" contentType="text/html;charset=utf-8" pageEncoding="utf-8" %>
+<!-- DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0.1" -->
+
 <html>
 <body>
 <h2>Hello World!</h2>
