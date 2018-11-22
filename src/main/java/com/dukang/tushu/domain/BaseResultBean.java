0eae92e06780766e9da6e30e1b06370fb719117e
a1593 <a1593@DESKTOP-VG5DJ57.lan>
2018-11-22 22:44:26 +0800

    添加工具类

---
+++ src/main/java/com/dukang/tushu/domain/BaseResultBean.java
@@ -0,0 +1,21 @@
+package com.dukang.tushu.domain;
+
+public class BaseResultBean {
+	private int code;
+	private String token;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
